bbbfb8e13ad10d655468176248901260fd3d9280
bedlansonny <bedlansonny@gmail.com>
2018-01-31 15:33:11 -0600

    removed leftDepth

--- src/Tree.java
+++ src/Tree.java
@@ -4,14 +4,12 @@ public class Tree
 {
     int[] leafCounts;
     int currentInd;
-    int leftDepth;
     HashMap<Integer, Integer> piles;    //<position, leafcount>
 
     public Tree(int[] leafCounts)
     {
         this.leafCounts = leafCounts;
         currentInd = 0;
-        leftDepth = 0;
         piles = new HashMap<>();
     }
 
@@ -36,7 +34,6 @@ public class Tree
         currentInd++;
         if(leafCounts[currentInd] == -1)
             return;
-        leftDepth++;
         parent.left = new Node(parent.pos-1, leafCounts[currentInd]);
         visit(parent.left);
     }
